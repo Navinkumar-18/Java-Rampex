ce3d904baf675e243f1872ff92745cb843633fe5
Navinkumar-18 <197935937+Navinkumar-18@users.noreply.github.com>
2026-09-10 15:02:18 +0530

    Day 3

---
+++ Array/evenodd.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class evenodd{
+    public static void main(String[] args)
+    {
+        
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            if(a[i]%2!=0){
+                for(int j=i;j<n;j++){
+                    if((a[j]%2)==0){
+                        int temp=a[i];
+                        a[i]=a[j];
+                        a[j]=temp;break;
+                    }
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(a[i]+" ");
+
+        }
+
+    }
+}
---
+++ Array/leftrotate.java
@@ -0,0 +1,24 @@
+package Array;
+import java.util.*;
+public class leftrotate {
+    public static void main(String[]args){
+
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+         int first=a[0];
+           
+        for(int i=0;i<n-1;i++){
+           a[i]=a[i+1];
+            }
+            a[n-1]=first;
+        for(int i=0;i<n;i++){
+            System.out.print(a[i]+" ");
+        }
+       
+
+    }
+}
