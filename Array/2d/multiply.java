a697abae9e25e8895f5ac9e3fa18a959f95d9135
Navinkumar-18 <197935937+Navinkumar-18@users.noreply.github.com>
2026-09-12 10:15:03 +0530

    2d arrays completed

--- Array/2d/multiply.java
+++ Array/2d/multiply.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class mul {
+public class multiply {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter size of first matrix:");
